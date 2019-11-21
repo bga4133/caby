a108b52ab718f1458ad8cf80d016d625922746da
Mac <mac@julito-MBP13.local>
2019-11-21 18:13:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+// import './App.css';
+import Products from './components/Products';
+import Basket from './components/Basket'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+  super(props);
+  this.state = { products: [], filteredProducts: [] , cartItems : [] };
+  this.handleAddToCart = this.handleAddToCart.bind(this);
+  this.handleRemoveFromCart = this.handleRemoveFromCart.bind(this);
+  }
+  componentWillMount(){
+    fetch("http://localhost:8000/products").then(res => res.json())
+    .then( data => this.setState({
+      products:data,
+      filteredProducts:data
+    }));
+    if(localStorage.getItem('cartItems')){
+      this.setState({cartItems: JSON.parse(localStorage.getItem('cartItems'))});
+    }
+  }
+  handleAddToCart(e, product){
+    this.setState(state =>{
+      const cartItems = state.cartItems;
+      let productAlreadyInCart = false ;
+      cartItems.forEach(item => {
+        if(item.id === product.id){
+          productAlreadyInCart =  true;
+          item.count++;
+        }
+      });
+      if(!productAlreadyInCart){
+        cartItems.push({...product, count:1});
+      }
+      localStorage.setItem("cartItems",JSON.stringify(cartItems));
+      return cartItems;
+    })
+  }
+  handleRemoveFromCart(e, item){
+    this.setState( state => {
+      const cartItems = state.cartItems.filter(elm => elm.id != item.id);
+      localStorage.setItem('cartItem',cartItems);
+      return {cartItems};
+    })
+  }
+  render(){
+    return (
+      <>
+        <div className="App">
+          <h1>Hola</h1>
+          <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+
+          <div>
+            <Basket cartItems={this.state.cartItems} handleRemoveFromCart={this.handleRemoveFromCart} />
+          </div>
+        </div> 
+
+
+
+        {/* <main class="App">
+          <section class="products">
+            <h1 class="main">Shopping cart</h1>
+            <ul class="products-list tableHead">
+              <li class="products-list-title row">
+                <div class="col-product">Product details</div>
+                <div class="col-quantity">Quantity</div>
+                <div class="col-price">Price</div>
+                <div class="col-total">Total</div>
+              </li>
+            </ul>
+            <ul class="products-list">
+              <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart} />
+            </ul>
+          </section>
+          <aside class="summary">
+            <Basket cartItems={this.state.cartItems} handleRemoveFromCart={this.handleRemoveFromCart} />
+          </aside>
+        </main>   */}
+      </>
+    ); 
+  }
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+export default class Products extends Component {
+    render() {
+        const productItems = this.props.products.map( product => (
+            <>
+                <a href={`#${product.id}`} onClick={(e)=> this.props.handleAddToCart(e, product)}>
+                    <img src={`/products/${product.sku}.png`} alt={product.title}></img>
+                    <p>{product.title}</p>
+                </a>
+                <div>
+                    <p>{product.price} €</p>
+                    <button className=""
+                    onClick={(e)=> this.props.handleAddToCart(e, product)}
+                    >Add to cart</button>
+                </div> 
+
+{/* 
+                <li class="product row">
+                        <div class="col-product">
+                        <figure class="product-image">
+                            <img src={`/products/${product.sku}.png`} alt={product.title} />
+                            <div class="product-description">
+                            <h1>{product.title}</h1>
+                            <p class="product-code">Product code X7R2OPX</p>
+                            </div>
+                        </figure>
+                        </div>
+                        <div class="col-quantity">
+                        <button class="count">-</button
+                        ><input type="text" class="product-quantity" value="3" /><button
+                            class="count"
+                        >
+                            +
+                        </button>
+                        </div>
+                        <div class="col-price">
+                        <span class="product-price">{product.price}</span
+                        ><span class="product-currency currency">€</span>
+                        </div>
+                        <div class="col-total">
+                        <span class="product-price">60</span
+                        ><span class="product-currency currency">€</span>
+                        </div>
+                    </li>  */}
+            </>  
+            )
+        )
+        return (
+            <div>
+                {productItems}
+            </div>
+        )
+    }
+}
